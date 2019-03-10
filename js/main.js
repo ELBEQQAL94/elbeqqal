6ec38d7bd991e234f0540463e78e4bab5029cd42
EL BEQQAL Youssef <ELBEQQAL.youssef@gmail.com>
2019-03-10 10:22:48 +0000

    modified smoothScroll function

--- js/main.js
+++ js/main.js
@@ -9,46 +9,17 @@ links.forEach(link => link.addEventListener('click', handlClass));
 // handlClass function
 function handlClass(e) {
     // Smooth Scroll
-    smoothScroll(e)
+    //smoothScroll(e)
     links.forEach(link => link.classList.remove('active'));
     e.currentTarget.classList.add('active');
     e.preventDefault();
 }
 
+// SmoothScroll
+var scroll = new SmoothScroll('.navbar a[href*="#"]', {
+    speed : 1000
+});
 
-/*Smooth scroll*/
-function smoothScroll(e) {
-    // Get target id 
-    const targetId = e.currentTarget.getAttribute('href');
-
-    // Get target position 
-    const targetPosition = document.querySelector(`${targetId}`).offsetTop;
-
-    // Get start position scroll
-    const startPosition = window.pageYOffset;
-
-    // Calc distance between start and position scroll
-    const distance = targetPosition - startPosition;
-
-    const duration = 1500;
-
-    let start = null
-
-    window.requestAnimationFrame(step);
-
-    function step(timestamp) {
-        if(!start) start = timestamp;
-        const progress = timestamp - start;
-        window.scrollTo(0,linear(distance,progress,duration,startPosition));
-
-        if(progress < duration) window.requestAnimationFrame(step);
-    }
-}
-
-// Linear function
-function linear(a, b, c, d) {
-   return a * (b / c) + d - 65;
-}
 
 // Insert current date into footer
 // Get current date
